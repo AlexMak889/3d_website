6f4af3eea23af5c9f5aa6030d775f09aa32d206c
alex <alexmak889@gmail.com>
2024-09-04 16:25:17 +0000

    not working comit from scholl laptop. tryed to import 3d model it is not loading

---
+++ src/components/Experience.jsx
@@ -0,0 +1,11 @@
+import { OrbitControls } from "@react-three/drei";
+import { Test } from "./Test";
+
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+      <Test /> 
+    </>
+  );
+};
