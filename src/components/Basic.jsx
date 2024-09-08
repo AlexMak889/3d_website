bdabe4d25f8102b993f7699aed0cf88ade311b40
alex <alexmak889@gmail.com>
2024-09-08 09:55:59 +0000

    not working imported moddel from blender but not working

---
+++ src/components/Basic.jsx
@@ -0,0 +1,16 @@
+//Test.jsx
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+                
+export function Model(props) {
+ const { nodes, materials } = useGLTF('models/scene.glb')
+ 
+ return (
+  <group {...props} dispose={null}>
+    <mesh geometry={nodes.Sphere.geometry} material={materials['Material.003']} position={[-2.318, 1.219, 2.344]} />
+    <mesh geometry={nodes.Cube.geometry} material={materials['Material.001']} position={[0.635, 1.454, -3.365]} />
+    <mesh geometry={nodes.Sphere001.geometry} material={materials['Material.002']} position={[-1.529, 2.411, -0.9]} />
+   </group>
+ )
+}
+useGLTF.preload('models /scene.glb');
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,8 @@
 import React, { useRef, useEffect } from 'react';
 import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { Model } from "./Test"; // Ensure this path is correct
 import { PointLight, PointLightHelper } from 'three';
+import { Room } from './Test';
 
 const LightWithHelper = () => {
   const pointLightRef = useRef();
@@ -36,7 +36,7 @@ export const Experience = () => {
     <Canvas style={{ width: '100%', height: '100%' }}>
       <ambientLight intensity={0.5} />
       <LightWithHelper />
-      <Model />
+      <Room />
       <OrbitControls />
     </Canvas>
   );
