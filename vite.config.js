f80cbfbdc2237d92e1358f2d36c4198b9065aaa0
alex <alexmak889@gmail.com>
2024-09-17 08:48:23 +0000

    test deploy chaenged vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://alexmak889.github.io/', // Replace with your GitHub repository name
+  base: '/',
 });
